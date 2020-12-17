2c198a0ba14c560edf852f6cf31412be68ab4d68
Leonardo Nicolai <nicolai.padilha@codengage.com>
2020-12-17 01:12:43 -0300

    add HeroCard component

---
+++ src/components/HeroCard/HeroCard.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+import Flex from "components/Flex";
+import Image from "components/Image";
+import Box from "components/Box";
+import Text from "components/Text";
+
+import heart from "assets/icones/heart/heart.svg";
+import heartFullfilled from "assets/icones/heart/heart-fullfilled.svg";
+
+export const HeroCard = ({
+  onClick,
+  hero,
+  favorite,
+  onClickFavorite,
+  ...props
+}) => {
+  return (
+    <Box onClick={onClick} my="15px" mx="10px" width="200px" height="250px">
+      <Flex flex={1}>
+        <Image
+          height="200px"
+          width="200px"
+          img={`${hero.thumbnail.path}.${hero.thumbnail.extension}`}
+        />
+      </Flex>
+      <Flex mt="15px" justifyContent="space-between">
+        <Text key={hero.id}>{hero.name}</Text>
+        <Image
+          onClick={onClickFavorite}
+          src={favorite ? heartFullfilled : heart}
+          width="20px"
+          height="20px"
+        />
+      </Flex>
+    </Box>
+  );
+};
