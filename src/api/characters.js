0612e7ec402e776fd7be790e854f1be63d5a605c
Leonardo Nicolai <nicolai.padilha@codengage.com>
2020-12-16 23:07:06 -0300

    update characters api

--- src/api/characters.js
+++ src/api/characters.js
@@ -1,10 +1,12 @@
 const axios = require("axios");
 
-export const getCharacters = ({ frase, offset }) =>
+export const getCharacters = ({ frase, offset, orderBy }) =>
   axios
     .get(
       `https://gateway.marvel.com:443/v1/public/characters?${
         frase && `name=${frase}&`
-      }offset=${offset}&apikey=${process.env.REACT_APP_API_KEY}`
+      }offset=${offset}&orderBy=${orderBy}&apikey=${
+        process.env.REACT_APP_API_KEY
+      }`
     )
     .then((data) => data.data.data);
